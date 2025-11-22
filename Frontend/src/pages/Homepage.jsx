a0927b99386b3826d97d127b5bc26a5abf065961
JeenaAanchal <jeenaaanchal@gmail.com>
2025-11-22 21:04:48 +0530

    HomePage updated

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -62,7 +62,7 @@ export default function HomePage() {
             <img
               src="/src/assets/homepage-pic3.webp"
               alt="Decor 3"
-              className="absolute w-60 h-40 top-50 right-1/2  -translate-x-1/2 shadow-2xl"
+              className="absolute w-60 h-40 top-50  right-10  -translate-x-1/2 shadow-2xl"
             />
 
             {/* Lower Right */}
